4a87443a6f2835765fc9be64cd2917aede25dcb2
Zhangj <1844699135@qq.com>
2022-09-07 16:21:42 +0800

    hot-fix commit1

--- src/main/java/com/zhangj/java/GitTest.java
+++ src/main/java/com/zhangj/java/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello world2");
         System.out.println("hello world3");
+        System.out.println("hello world4");
     }
 }
