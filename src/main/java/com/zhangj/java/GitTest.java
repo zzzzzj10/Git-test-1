948447bdc7c5656bba4033a8529571296714ee98
Zhangj <1844699135@qq.com>
2022-09-07 16:02:51 +0800

    first commit

---
+++ src/main/java/com/zhangj/java/GitTest.java
@@ -0,0 +1,12 @@
+package com.zhangj.java;
+
+/**
+ * @author ZhangJ
+ * @Describe
+ * @create 2022-09-07-15:55
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
