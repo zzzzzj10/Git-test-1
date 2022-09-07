77675030318ae45fc3f51df3f791662f43570127
Zhangj <1844699135@qq.com>
2022-09-07 16:34:05 +0800

    test commit

--- src/main/java/com/zhangj/java/GitTest.java
+++ src/main/java/com/zhangj/java/GitTest.java
@@ -11,5 +11,8 @@ public class GitTest {
         System.out.println("hello world2");
         System.out.println("hello world3");
         System.out.println("hello world4");
+        System.out.println("master test");
+
+
     }
 }
