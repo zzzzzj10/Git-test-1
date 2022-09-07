8ccfcece9bee5726042f7dc475d46d476c13b890
Zhangj <1844699135@qq.com>
2022-09-07 16:11:17 +0800

    third commit

--- src/main/java/com/zhangj/java/GitTest.java
+++ src/main/java/com/zhangj/java/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("hello world2");
+        System.out.println("hello world3");
     }
 }
