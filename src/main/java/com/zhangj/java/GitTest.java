0439ea61cc2b5fef24bbfc7fd91caebc9d79a44c
Zhangj <1844699135@qq.com>
2022-09-07 16:08:44 +0800

    second commit

--- src/main/java/com/zhangj/java/GitTest.java
+++ src/main/java/com/zhangj/java/GitTest.java
@@ -8,5 +8,6 @@ package com.zhangj.java;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
